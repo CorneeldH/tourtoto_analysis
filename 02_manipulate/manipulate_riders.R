66706320939d9d5b38887412030441041a8840cf
Corneel den Hartogh <c.f.den.hartogh@vu.nl>
2023-07-08 21:07:35 +0200

    feat: add multiplier (horses) to riders

--- 02_manipulate/manipulate_riders.R
+++ 02_manipulate/manipulate_riders.R
@@ -16,7 +16,36 @@ riders_df <- read_rds("C:/Users/user/Dropbox/Programming/Tourtoto/overig/data/01
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## 2. MODIFY ####
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-## Pas kolomnamen aan met behulp van de documentatie
+
+# Years have different cut-off points for multiplier (horses)
+riders_2022 <- riders_df %>%
+  filter(year_int >= 2022) %>%
+  map_num_to_num("gekozen", "multiplier_0_2_8_23")
+
+riders_2020 <- riders_df %>%
+  filter(year_int %in% c(2020, 2021)) %>%
+  map_num_to_num("gekozen", "multiplier_0_2_6_16")
+
+
+riders_before_2020 <- riders_df %>%
+  filter(year_int < 2020) %>%
+  mutate(multiplier = 1)
+
+
+# Combine them again
+riders <- bind_rows(riders_2022,
+                    riders_2020,
+                    riders_before_2020)
+
+# Get it all in one column and remove the temporary columns
+multiplier_columns <- riders %>%
+  select(starts_with("multiplier")) %>%
+  names()
+
+
+riders <- riders %>%
+  mutate(multiplier = coalesce(!!!syms(multiplier_columns))) %>%
+  select(!(starts_with("multiplier") & !ends_with("multiplier")))
 
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -24,7 +53,7 @@ riders_df <- read_rds("C:/Users/user/Dropbox/Programming/Tourtoto/overig/data/01
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 write_rds(
-  riders_df,
+  riders,
   "C:/Users/user/Dropbox/Programming/Tourtoto/overig/data/02_manipulated/RID_riders.rds"
 )
 
