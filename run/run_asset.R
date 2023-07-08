3916d8602bcbdb686cede32bc9ba8052f78659cb
Corneel den Hartogh <c.f.den.hartogh@vu.nl>
2023-07-08 19:06:44 +0200

    source: all

--- run/run_asset.R
+++ run/run_asset.R
@@ -1,8 +1,6 @@
 
-source("01_validate/validate_participants.R")
-source("01_validate/validate_riders.R")
 
-source("02_manipulate/manipulate_participants.R")
-source("02_manipulate/manipulate_riders.R")
+folders <- list.files(pattern = "^0", full.names = TRUE)
 
-source("03_assetize/assetize.R")
+walk(folders, ~list.files(., full.names = TRUE) %>% walk(source))
+
